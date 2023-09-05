51dfdecf771afdcee402491d5b1013af0f5f420e
meenatchi <142766327+meenatchi-14@users.noreply.github.com>
2023-09-05 06:51:54 +0530

    Update rotate.js

--- function/rotate.js
+++ function/rotate.js
@@ -1,15 +1,15 @@
 console.log("------------------------------------------");
 console.log("Rotate an array by k times");
-
-const rotateArray1 = function(nums, k) {
-
+var array=[1,2,3,4,5,6];
+var n=2;
+(function(){                                 // IIFE FUNCTION
+  const rotateArray1 = function(nums, k) {   // anonymous function
     for (let i = 0; i < k; i++) {
         nums.unshift(nums.pop());
     }
-  
     return nums;
   }
-  const rotateArray2 = function(nums, k) {
+  const rotateArray2 = function(nums, k) {   // anonymous function
     function reverse(arr, start, end) {
       while (start < end) {
         [arr[start], arr[end]] = [arr[end], arr[start]];
@@ -23,6 +23,5 @@ const rotateArray1 = function(nums, k) {
     reverse(nums, k, (nums.length - 1));
     return nums;
   }
-  var array=[1,2,3,4,5,6];
-  var n=2;
  console.log(rotateArray2(array,n));
+})();
